7af7269b5d2e45a6a0333c03a699870b09f8e9be
landroide13 <landrade1313@gmail.com>
2021-10-22 12:21:46 +1300

    act4

---
+++ act4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace act4
+{
+    class Program
+    {
+
+        static void setTable(int num)
+        {
+            int[] table = new int[10];
+
+            for (int i = 0; i <= table.Length; i++)
+            {
+                Console.WriteLine(i + " x " + num + " = " + (num * i));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("########## Multiplication Table ################");
+            Console.WriteLine("########## Add a Number ################");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            setTable(number);
+
+        }
+    }
+}
