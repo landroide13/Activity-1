2623afa981bc784f432489c08ee62dc0ccaf96e9
landroide13 <landrade1313@gmail.com>
2021-10-22 12:06:00 +1300

    act3 done

---
+++ act3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace act3
+{
+    class Program
+    {
+         static void setArr()
+        {
+            int[] arr = new int[10];
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                Console.WriteLine("###### Add Elements ######");
+                Console.WriteLine("Element: " + " " + i);
+                int el = Convert.ToInt32(Console.ReadLine());
+                arr[i] = el;
+
+            }
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(" Ele " + " " + i + ":  " + arr[i]);
+
+                sum += arr[i];
+            }
+
+            Console.WriteLine("The Sum: " + sum);
+            Console.WriteLine("The Avg: " + sum / 10);
+
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("######## Enter 10 numbers ############");
+
+            setArr();
+        }
+    }
+}
