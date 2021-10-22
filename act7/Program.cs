02454f8f6ac4789b6bb9bf66c25bd58ab84b4166
landroide13 <landrade1313@gmail.com>
2021-10-22 14:47:51 +1300

    act7

---
+++ act7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace act7
+{
+    class Program
+    {
+
+        static double perimeter(int rad)
+        {
+            return 2 * 3.14 * rad;
+        }
+
+        static double area(int rad)
+        {
+            return (rad * rad) * 3.14;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("######## Circle App #########");
+            Console.WriteLine("######## Please add the radius: ");
+
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Area is: " + area(radius));
+            Console.WriteLine("The Perimeter is: " + perimeter(radius));
+        }
+    }
+}
