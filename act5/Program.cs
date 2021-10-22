9817fafb3021ce3e38d10c74e40cfd8cf5cbe336
landroide13 <landrade1313@gmail.com>
2021-10-22 14:20:19 +1300

    act5 done

---
+++ act5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace act5
+{
+    class Program
+    {
+        static void operations(int a, int b)
+        {
+            Console.WriteLine("The Sum of: " + a + " + " + b + " = " + (a + b));
+            Console.WriteLine("The Mult of: " + a + " * " + b + " = " + (a * b));
+            Console.WriteLine("The Subs of: " + a + " - " + b + " = " + (a - b));
+            Console.WriteLine("The Div of: " + a + " / " + b + " = " + (a / b));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("########## Operation App ################");
+            Console.WriteLine("########## Add the First Number ################");
+
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("########## Add the Second Number ################");
+
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            operations(firstNum, secondNum);
+
+        }
+    }
+}
